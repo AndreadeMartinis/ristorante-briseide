cc8d9dbe2b4d28af0f93925cb64c973f843423ee
Andrea de Martinis <andreademartinis86@gmail.com>
2025-04-04 14:49:03 +0200

    Ripristinato il background fixed(parallax) per la versione desktop

--- app/components/Motto.js
+++ app/components/Motto.js
@@ -7,7 +7,7 @@ export default function Motto() {
   return (
     <section
       id="motto"
-      className="h-screen w-full bg-[url('/img/bg-gray-blue.jpg')] bg-cover bg-center flex flex-col justify-center items-center font-cursive text-5xl md:text-[8rem] text-secondary text-center"
+      className="h-screen w-full bg-[url('/img/bg-gray-blue.jpg')] md:bg-fixed bg-cover bg-center flex flex-col justify-center items-center font-cursive text-5xl md:text-[8rem] text-secondary text-center"
     >
       <h2>{t("homepage.motto1")}</h2>
       <h2>{t("homepage.motto2")}</h2>
