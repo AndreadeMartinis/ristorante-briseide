f109f6cf92d11a1ba97887a58e9abf4418b5bc7c
Andrea de Martinis <andreademartinis86@gmail.com>
2025-04-08 16:32:14 +0200

    Modifiche al layout menu pasquale

--- app/components/restaurantMenu/RowSpecial.js
+++ app/components/restaurantMenu/RowSpecial.js
@@ -12,7 +12,7 @@ export default function RowSpecial({ name, details, language }) {
 
   return (
     <div className="bordered-box rowspecial flex flex-col justify-center text-center border-b py-8 last-of-type:border-none last-of-type:outline-none w-full md:w-3/4">
-      <h3 className="font-semibold text-xl md:text-3xl pb-2 uppercase">
+      <h3 className="font-medium text-2xl md:text-3xl  uppercase mb-2 tracking-widest border-b border-primary w-fit m-auto">
         {label}
       </h3>
       {items.map((item, index) => (
